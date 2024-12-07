cf2e2de2d37fd4b80de581574954667f4988ab2d
Joery <44531907+Joery-M@users.noreply.github.com>
2024-12-07 16:59:26 +0100

    refactor: :recycle: Moved to monorepo style and added rust project

--- packages/website/src/App.vue
+++ packages/website/src/App.vue
@@ -4,6 +4,11 @@
 
 <script setup lang="ts">
 import { useDark } from '@vueuse/core';
+import { greet } from 'vp9-binding';
+import { onMounted } from 'vue';
 
+onMounted(async () => {
+    greet();
+});
 useDark({ storageKey: 'color' });
 </script>
--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import mkcert from 'vite-plugin-mkcert';
+import wasm from 'vite-plugin-wasm';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [mkcert(), vue()]
+    plugins: [mkcert(), vue(), wasm()]
 });
